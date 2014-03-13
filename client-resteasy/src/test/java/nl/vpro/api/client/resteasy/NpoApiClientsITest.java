e465eb4f9d8ae561e58c87bcfd10edd3312acca3
soarten <soarten@vpro.nl>
2014-03-13 12:04:01 +0000

    NPA-1 Clint with test cases

--- client-resteasy/src/test/java/nl/vpro/api/client/resteasy/NpoApiClientsITest.java
+++ client-resteasy/src/test/java/nl/vpro/api/client/resteasy/NpoApiClientsITest.java
@@ -4,11 +4,17 @@
  */
 package nl.vpro.api.client.resteasy;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotAuthorizedException;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import nl.vpro.api.rs.v3.media.MediaRestService;
 import nl.vpro.domain.api.MediaResult;
+import nl.vpro.domain.api.MediaSearchResult;
+import nl.vpro.domain.api.media.MediaForm;
+import nl.vpro.domain.api.media.MediaFormBuilder;
 import nl.vpro.domain.media.MediaObject;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -32,6 +38,23 @@ public class NpoApiClientsITest {
         );
     }
 
+    @Test(expected = NotAuthorizedException.class)
+    public void testAccessForbidden() throws Exception {
+        clients = new NpoApiClients(
+            "http://rs-dev.poms.omroep.nl/v1/api/",
+            "ione7ahfij",
+            "WRONG_PASSWORD",
+            "http://www.vpro.nl"
+        );
+
+        clients.getMediaService().list(null, null, null, null);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testNotFound() throws Exception {
+        clients.getMediaService().load("DOES_NOT_EXIST", null);
+    }
+
     @Test
     public void testMediaServiceLists() throws Exception {
         MediaRestService mediaService = clients.getMediaService();
@@ -50,6 +73,26 @@ public class NpoApiClientsITest {
         assertThat(mediaService.listMembers(mid, null, null, null, null)).isNotNull();
 
         assertThat(mediaService.listDescendants(mid, null, null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void testMediaServiceFinds() throws Exception {
+        MediaRestService mediaService = clients.getMediaService();
+        MediaForm form = MediaFormBuilder.form().broadcasters("VPRO").broadcasterFacet().build();
+
+        MediaSearchResult list = mediaService.find(form, null, "none", null, null);
+        assertThat(list).isNotEmpty().hasSize(10);
+        assertThat(list.getFacets().getBroadcasters()).isNotEmpty();
+
+        String mid = list.getItems().get(0).getResult().getMid();
+
+        assertThat(mediaService.findEpisodes(form, mid, null, null, null, null)).isNotNull();
+
+        assertThat(mediaService.findMembers(form, mid, null, null, null, null)).isNotNull();
+
+        assertThat(mediaService.findDescendants(form, mid, null, null, null, null)).isNotNull();
+
+// TODO enable        assertThat(mediaService.findRelated(form, mid, null, null, null)).isNotNull();
     }
 
     @Test
