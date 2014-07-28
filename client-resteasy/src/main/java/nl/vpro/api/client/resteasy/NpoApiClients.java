a31adb6fade30b257cda27e6f9b717a311b63961
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-07-28 15:03:53 +0000

    simplified;

--- client-resteasy/src/main/java/nl/vpro/api/client/resteasy/NpoApiClients.java
+++ client-resteasy/src/main/java/nl/vpro/api/client/resteasy/NpoApiClients.java
@@ -14,12 +14,13 @@ import javax.ws.rs.core.MediaType;
 public class NpoApiClients extends AbstractApiClient {
     private final NpoApiAuthentication authentication;
 
-    private MediaRestService mediaRestServiceProxy;
+    private final MediaRestService mediaRestServiceProxy;
 
-    private PageRestService pageRestServiceProxy;
+    private final PageRestService pageRestServiceProxy;
 
-    private ProfileRestService profileRestServiceProxy;
+    private final ProfileRestService profileRestServiceProxy;
 
+	private final String baseUrl;
 
     @Inject
     public NpoApiClients(
@@ -28,18 +29,12 @@ public class NpoApiClients extends AbstractApiClient {
         @Named("npo-api.secret") String secret,
         @Named("npo-api.origin") String origin
     ) {
-        this(apiBaseUrl, new NpoApiAuthentication(apiKey, secret, origin), 10000, 16, 10000);
-    }
-
-    private NpoApiClients(String apiBaseUrl, NpoApiAuthentication authentication, int connectionTimeoutMillis, int maxConnections, int connectionInPoolTTL) {
-        super(connectionTimeoutMillis, maxConnections, connectionInPoolTTL);
-
-        this.authentication = authentication;
-
-        String baseUrl = apiBaseUrl + "api";
-        initMediaRestServiceProxy(baseUrl);
-        initPageRestServiceProxy(baseUrl);
-        initProfileRestServiceProxy(baseUrl);
+		super(10000, 16, 10000);
+        this.authentication = new NpoApiAuthentication(apiKey, secret, origin);
+        baseUrl = apiBaseUrl + "api";
+		mediaRestServiceProxy = getTarget(baseUrl).proxyBuilder(MediaRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
+		pageRestServiceProxy = getTarget(baseUrl).proxyBuilder(PageRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
+		profileRestServiceProxy = getTarget(baseUrl).proxyBuilder(ProfileRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
     }
 
     public MediaRestService getMediaService() {
@@ -59,15 +54,9 @@ public class NpoApiClients extends AbstractApiClient {
         return client.target(url);
     }
 
-    private void initMediaRestServiceProxy(String url) {
-        mediaRestServiceProxy = getTarget(url).proxyBuilder(MediaRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
-    }
+	@Override
+	public String toString() {
+		return super.toString() + " " + baseUrl;
+	}
 
-    private void initPageRestServiceProxy(String url) {
-        pageRestServiceProxy = getTarget(url).proxyBuilder(PageRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
-    }
-
-    private void initProfileRestServiceProxy(String url) {
-        pageRestServiceProxy = getTarget(url).proxyBuilder(PageRestService.class).defaultConsumes(MediaType.APPLICATION_JSON).build();
-    }
 }
