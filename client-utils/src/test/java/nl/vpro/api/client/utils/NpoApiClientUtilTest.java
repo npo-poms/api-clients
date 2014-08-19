549061ae4d22afc4e74b3a2bc24fb2261193e84d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-08-19 17:50:18 +0000

    Test for utils.

---
+++ client-utils/src/test/java/nl/vpro/api/client/utils/NpoApiClientUtilTest.java
@@ -0,0 +1,40 @@
+package nl.vpro.api.client.utils;
+
+import java.net.MalformedURLException;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import nl.vpro.api.client.resteasy.NpoApiClients;
+import nl.vpro.domain.media.MediaObject;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@Ignore("This is an integration test")
+public class NpoApiClientUtilTest {
+
+    private NpoApiClientUtil util;
+
+    //private String target = "http://rs-dev.poms.omroep.nl/v1/";
+    private String target = "http://localhost:8070/v1/";
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        NpoApiClients clients = new NpoApiClients(
+            target,
+            "ione7ahfij",
+            "***REMOVED***",
+            "http://www.vpro.nl");
+        util = new NpoApiClientUtil(clients);
+    }
+
+
+    @Test
+    public void testLoad() throws Exception {
+        MediaObject[] result = util.load("AVRO_1656037", "POMS_VPRO_487567");//, "AVRO_1656037");
+        assertThat(result[0].getMid()).isEqualTo("AVRO_1656037");
+        assertThat(result[1].getMid()).isEqualTo("POMS_VPRO_487567");
+        assertThat(result[2].getMid()).isEqualTo("AVRO_1656037");
+    }
+}
