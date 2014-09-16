e8a5f7abb697ac0b5f172291b96ce31a9435d0e2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-09-16 16:50:39 +0000

    API-160

---
+++ client-utils/src/main/java/nl/vpro/api/client/utils/ApiClientScheduleRepository.java
@@ -0,0 +1,56 @@
+package nl.vpro.api.client.utils;
+
+import java.util.Date;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import nl.vpro.api.client.resteasy.NpoApiClients;
+import nl.vpro.domain.api.Order;
+import nl.vpro.domain.api.ScheduleResult;
+import nl.vpro.domain.api.media.ScheduleRepository;
+import nl.vpro.domain.media.Channel;
+import nl.vpro.domain.media.Net;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.1
+ */
+@Named
+public class ApiClientScheduleRepository implements ScheduleRepository {
+
+    final NpoApiClients clients;
+    final NpoApiClientUtil util;
+
+    @Inject
+    public ApiClientScheduleRepository(NpoApiClients clients) {
+        this.clients = clients;
+        this.util = new NpoApiClientUtil(clients);
+    }
+
+    @Override
+    public ScheduleResult listSchedules(Date start, Date stop, Order order, Long offset, int max) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ScheduleResult listSchedules(Channel channel, Date start, Date stop, Order order, Long offset, int max) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ScheduleResult listSchedules(Net net, Date start, Date stop, Order order, Long offset, int max) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ScheduleResult listSchedulesForBroadcaster(String broadcaster, Date start, Date stop, Order order, Long offset, int max) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public ScheduleResult listSchedulesForAncestor(String mediaId, Date start, Date stop, Order order, Long offset, int max) {
+        throw new UnsupportedOperationException();
+    }
+}
