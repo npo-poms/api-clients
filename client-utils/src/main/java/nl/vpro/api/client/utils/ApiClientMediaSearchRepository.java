816c87b6701ae18cdb17d95c998f598caaa65a0a
soarten <soarten@vpro.nl>
2014-09-24 09:17:25 +0000

    MGNL-11061 Prohibit null arguments on VPRO ApiClientRepository

--- client-utils/src/main/java/nl/vpro/api/client/utils/ApiClientMediaSearchRepository.java
+++ client-utils/src/main/java/nl/vpro/api/client/utils/ApiClientMediaSearchRepository.java
@@ -8,6 +8,7 @@ import nl.vpro.domain.api.MediaSearchResult;
 import nl.vpro.domain.api.ProgramSearchResult;
 import nl.vpro.domain.api.ScheduleSearchResult;
 import nl.vpro.domain.api.media.MediaForm;
+import nl.vpro.domain.api.media.MediaFormBuilder;
 import nl.vpro.domain.api.media.MediaSearchRepository;
 import nl.vpro.domain.api.profile.ProfileDefinition;
 import nl.vpro.domain.media.MediaObject;
@@ -26,29 +27,29 @@ public class ApiClientMediaSearchRepository extends AbstractApiClientMediaReposi
 
     @Override
     public MediaSearchResult find(ProfileDefinition<MediaObject> profile, MediaForm form, Long offset, Integer max) {
-        return clients.getMediaService().find(form, profile != null ? profile.getName() : null, null, offset, max);
+        return clients.getMediaService().find(form != null ? form : MediaFormBuilder.emptyForm(), profile != null ? profile.getName() : null, null, offset, max);
 
     }
 
     @Override
     public MediaSearchResult findMembers(MediaObject media, ProfileDefinition<MediaObject> profile, MediaForm form, Long offset, Integer max) {
-        return clients.getMediaService().findMembers(form, media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
+        return clients.getMediaService().findMembers(form != null ? form : MediaFormBuilder.emptyForm(), media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
 
     }
 
     @Override
     public ProgramSearchResult findEpisodes(MediaObject media, ProfileDefinition<MediaObject> profile, MediaForm form, Long offset, Integer max) {
-        return clients.getMediaService().findEpisodes(form, media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
+        return clients.getMediaService().findEpisodes(form != null ? form : MediaFormBuilder.emptyForm(), media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
     }
 
     @Override
     public MediaSearchResult findDescendants(MediaObject media, ProfileDefinition<MediaObject> profile, MediaForm form, Long offset, Integer max) {
-        return clients.getMediaService().findDescendants(form, media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
+        return clients.getMediaService().findDescendants(form != null ? form : MediaFormBuilder.emptyForm(), media.getMid(), profile != null ? profile.getName() : null, null, offset, max);
     }
 
     @Override
     public MediaSearchResult findRelated(MediaObject media, ProfileDefinition<MediaObject> profile, MediaForm form, Integer max) {
-        return clients.getMediaService().findRelated(form, media.getMid(), profile != null ? profile.getName() : null, null, max);
+        return clients.getMediaService().findRelated(form != null ? form : MediaFormBuilder.emptyForm(), media.getMid(), profile != null ? profile.getName() : null, null, max);
     }
 
     @Override
