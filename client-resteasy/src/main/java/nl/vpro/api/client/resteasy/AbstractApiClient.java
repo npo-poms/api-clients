a4a19120d8d2591dcbb396e78b6d858b696d0722
soarten <soarten@vpro.nl>
2014-10-21 14:49:19 +0000

    MGNL-11312 Watch idle connections in separate thread

--- client-resteasy/src/main/java/nl/vpro/api/client/resteasy/AbstractApiClient.java
+++ client-resteasy/src/main/java/nl/vpro/api/client/resteasy/AbstractApiClient.java
@@ -6,7 +6,10 @@ package nl.vpro.api.client.resteasy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PreDestroy;
 
 import org.apache.http.Header;
 import org.apache.http.HeaderElement;
@@ -16,6 +19,7 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -29,6 +33,7 @@ import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 import nl.vpro.resteasy.JacksonContextResolver;
+import nl.vpro.util.ThreadPools;
 
 /**
  * @author Roelof Jan Koekoek
@@ -54,8 +59,13 @@ public class AbstractApiClient {
 
     protected final ClientHttpEngine clientHttpEngine;
 
+    private Thread connectionGuard;
+
+    private boolean shutdown = true;
+
     public AbstractApiClient(int connectionTimeoutMillis, int maxConnections, int connectionInPoolTTL) {
         clientHttpEngine = buildHttpEngine(connectionTimeoutMillis, maxConnections, connectionInPoolTTL);
+
     }
 
     protected ApacheHttpClient4Engine buildHttpEngine(int connectionTimeoutMillis, int maxConnections, int connectionInPoolTTL) {
@@ -63,10 +73,11 @@ public class AbstractApiClient {
         poolingClientConnectionManager.setDefaultMaxPerRoute(maxConnections);
         poolingClientConnectionManager.setMaxTotal(maxConnections);
 
+        watchIdleConnections(poolingClientConnectionManager, connectionTimeoutMillis);
 
         RequestConfig defaultRequestConfig = RequestConfig.custom()
             .setExpectContinueEnabled(true)
-            .setStaleConnectionCheckEnabled(true)
+            .setStaleConnectionCheckEnabled(false)
             .setMaxRedirects(100)
             .setConnectionRequestTimeout(connectionTimeoutMillis)
             .setConnectTimeout(connectionTimeoutMillis)
@@ -79,40 +90,74 @@ public class AbstractApiClient {
             .setConnectionManager(poolingClientConnectionManager)
             .setDefaultRequestConfig(defaultRequestConfig)
             .setDefaultHeaders(defaultHeaders)
-            //.setKeepAliveStrategy(new MyConnectionKeepAliveStrategy())
+                //.setKeepAliveStrategy(new MyConnectionKeepAliveStrategy())
             .build();
 
         return new ApacheHttpClient4Engine(client);
     }
 
-    private class MyConnectionKeepAliveStrategy implements  ConnectionKeepAliveStrategy {
+    @PreDestroy
+    public void shutdown() {
+        if(!shutdown) {
+            shutdown = true;
+            notifyAll();
+            connectionGuard = null;
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        shutdown();
+        super.finalize();
+    }
+
+    private class MyConnectionKeepAliveStrategy implements ConnectionKeepAliveStrategy {
 
         @Override
         public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
 
-            HttpRequestWrapper wrapper = (HttpRequestWrapper) context.getAttribute(HttpClientContext.HTTP_REQUEST);
-            if (wrapper.getURI().getPath().endsWith("/media/changes")) {
+            HttpRequestWrapper wrapper = (HttpRequestWrapper)context.getAttribute(HttpClientContext.HTTP_REQUEST);
+            if(wrapper.getURI().getPath().endsWith("/media/changes")) {
                 // 30 minutes
                 return 30 * 60 * 1000;
             }
             // Honor 'keep-alive' header
             HeaderElementIterator it = new BasicHeaderElementIterator(
                 response.headerIterator(HTTP.CONN_KEEP_ALIVE));
-            while (it.hasNext()) {
+            while(it.hasNext()) {
                 HeaderElement he = it.nextElement();
                 String param = he.getName();
                 String value = he.getValue();
-                if (value != null && param.equalsIgnoreCase("timeout")) {
+                if(value != null && param.equalsIgnoreCase("timeout")) {
                     try {
                         // as responded
                         return Long.parseLong(value) * 1000;
-                    } catch (NumberFormatException ignore) {
+                    } catch(NumberFormatException ignore) {
                     }
                 }
             }
             // 1 minute
             return 60 * 1000;
         }
+    }
 
-    };
+    private void watchIdleConnections(final HttpClientConnectionManager connectionManager, final int connectionTimeoutMillis) {
+        ThreadFactory threadFactory = ThreadPools.createThreadFactory("API Client purge idle connections", false, Thread.NORM_PRIORITY);
+        connectionGuard = threadFactory.newThread(new Runnable() {
+            @Override
+            public void run() {
+                while(!shutdown) {
+                    try {
+                        synchronized(this) {
+                            wait(5000);
+                            connectionManager.closeExpiredConnections();
+                            connectionManager.closeIdleConnections(connectionTimeoutMillis, TimeUnit.MILLISECONDS);
+                        }
+                    } catch(InterruptedException ex) {
+                    }
+                }
+            }
+        });
+        connectionGuard.run();
+    }
 }
