8635ba2b9623ccf56d077caf351d52d902ae50b2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-03-12 12:47:42 +0000

    More client implementations of repositories.

---
+++ client-utils/src/main/java/nl/vpro/api/client/utils/ApiClientPageQuerySearchRepository.java
@@ -0,0 +1,34 @@
+package nl.vpro.api.client.utils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import nl.vpro.api.client.resteasy.NpoApiClients;
+import nl.vpro.domain.api.SuggestResult;
+import nl.vpro.domain.api.suggest.Query;
+import nl.vpro.domain.api.suggest.QuerySearchRepository;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.10
+ */
+@Named("pageQueryRepository")
+public class ApiClientPageQuerySearchRepository implements QuerySearchRepository {
+
+    final NpoApiClients clients;
+
+    @Inject
+    public ApiClientPageQuerySearchRepository(NpoApiClients clients) {
+        this.clients = clients;
+    }
+
+    @Override
+    public void index(Query query, Type type) {
+
+    }
+
+    @Override
+    public SuggestResult suggest(String input, String profile, Integer max) {
+        return clients.getPageService().suggest(input, profile, max);
+    }
+}
