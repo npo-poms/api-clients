c0403c8f202c0082378d3c4fdcf022a7efecda3e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-09-08 16:25:35 +0000

    added straightforward profile implementation based on xmls

---
+++ client-utils/src/main/java/nl/vpro/api/client/utils/XmlProfileServiceImpl.java
@@ -0,0 +1,63 @@
+package nl.vpro.api.client.utils;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXB;
+
+import nl.vpro.domain.api.profile.Profile;
+import nl.vpro.domain.api.profile.ProfileDefinition;
+import nl.vpro.domain.api.profile.ProfileService;
+import nl.vpro.domain.media.MediaObject;
+import nl.vpro.domain.page.Page;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.17
+ */
+public class XmlProfileServiceImpl implements ProfileService {
+
+    private final Map<String, Profile> profiles = new HashMap<>();
+
+    public XmlProfileServiceImpl(InputStream... inputStreams) {
+        for (InputStream inputStream : inputStreams) {
+            if (inputStream != null) {
+                Profile profile = JAXB.unmarshal(inputStream, Profile.class);
+                profiles.put(profile.getName(), profile);
+            }
+        }
+    }
+
+    @Override
+    public Profile getProfile(String name) {
+        return profiles.get(name);
+
+    }
+
+    @Override
+    public Profile getProfile(String name, Date on) {
+        return getProfile(name);
+
+    }
+
+    @Override
+    public ProfileDefinition<Page> getPageProfileDefinition(String name) {
+        Profile profile = getProfile(name);
+        return profile == null ? null : profile.getPageProfile();
+
+    }
+
+    @Override
+    public ProfileDefinition<MediaObject> getMediaProfileDefinition(String name) {
+        Profile profile = getProfile(name);
+        return profile == null ? null : profile.getMediaProfile();
+
+    }
+
+    @Override
+    public ProfileDefinition<MediaObject> getMediaProfileDefinition(String name, Long since) {
+        return getMediaProfileDefinition(name);
+    }
+}
