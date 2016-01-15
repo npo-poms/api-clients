36a4f816d458ede85344e0cff29136a29dc9ac25
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-01-15 23:12:54 +0000

    backport

--- client-resteasy/src/main/java/nl/vpro/api/client/resteasy/ErrorAspect.java
+++ client-resteasy/src/main/java/nl/vpro/api/client/resteasy/ErrorAspect.java
@@ -1,19 +1,16 @@
 package nl.vpro.api.client.resteasy;
 
-import java.io.InputStream;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-
-import org.apache.commons.io.IOUtils;
-import org.jboss.resteasy.client.jaxrs.internal.ClientResponse;
 import org.slf4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -22,10 +19,10 @@ import nl.vpro.domain.api.Error;
 import nl.vpro.jackson2.Jackson2Mapper;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toCollection;
 
 /**
  * Wraps all calls to log client errors.
+ *
  * @author Michiel Meeuwissen
  * @since 4.2
  */
@@ -47,38 +44,61 @@ public class ErrorAspect<T> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Throwable t;
+        String mes;
         try {
             try {
                 return method.invoke(proxied, args);
             } catch (InvocationTargetException itc) {
-                Throwable cause = itc.getCause();
-                throw cause;
+                throw itc.getCause();
             }
-        } catch (WebApplicationException b) {
-            String mes;
-            try {
-                Response response = b.getResponse();
-                response.bufferEntity();
-                try {
-                    Error error = response.readEntity(Error.class);
-                    mes = error.toString();
-                } catch (Exception e) {
-                    String m = response.readEntity(String.class);
-                    mes = response.getStatus() + ":" + m;
-                }
-            } catch (Exception e) {
-                log.warn(e.getClass() + " " + e.getMessage());
-                mes = b.getMessage();
-            }
-
-
-            log.error("Bad request for {}{}(\n{}\n) {}",
-                string.get(),
-                method.getDeclaringClass().getSimpleName() + "#" + method.getName(),
-                Arrays.asList(args).stream().map(ErrorAspect.this::valueToString).collect(joining("\n")),
-                mes);
-            throw b;
+        } catch (WebApplicationException wea) {
+            mes = getMessage(wea);
+            t = wea;
+        } catch (Throwable e) {
+            mes = e.getClass().getName() + " " + e.getMessage();
+            t = e;
         }
+        log.error("Error for {}{}(\n{}\n) {}",
+            string.get(),
+            method.getDeclaringClass().getSimpleName() + "#" + method.getName(),
+            Arrays.asList(args).stream().map(ErrorAspect.this::valueToString).collect(joining("\n")),
+            mes);
+        throw t;
+    }
+
+    protected static final String[] HEADERS = new String[] {
+        "Set-Cookie", // may give information about which backend server was used
+        "X-ProxyInstancename",
+        "Content-Type" // problem may be related to json vs xml?
+    };
+    protected String getMessage(WebApplicationException we) {
+        StringBuilder mes = new StringBuilder();
+        try {
+            Response response = we.getResponse();
+            response.bufferEntity();
+            try {
+                Error error = response.readEntity(Error.class);
+                mes.append(error.toString());
+            } catch (Exception e) {
+                String m = response.readEntity(String.class);
+                mes.append(response.getStatus()).append(':').append(m);
+            }
+            for (String s : HEADERS) {
+                List<Object> v = response.getMetadata().get(s);
+                if (v != null) {
+                    mes.append("; ");
+                    mes.append(s);
+                    mes.append('=');
+                    mes.append(v.stream().map(Objects::toString).collect(joining(",")));
+                }
+            }
+        } catch (Exception e) {
+            log.warn(e.getClass() + " " + e.getMessage());
+            mes.append(we.getMessage());
+        }
+
+        return mes.toString();
     }
 
     protected String valueToString(Object o) {
