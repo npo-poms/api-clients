1e4701671dd2235c8902da40c08068e8e23bb2c6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-04-21 13:52:39 +0000

    backport

---
+++ media-backend-rs-client/src/main/java/nl/vpro/rs/media/ResponseError.java
@@ -0,0 +1,35 @@
+package nl.vpro.rs.media;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 4.3.2
+ */
+public class ResponseError extends RuntimeException {
+
+    private final int status;
+    private final Response.StatusType statusInfo;
+    private final String entity;
+
+    public ResponseError(int status, Response.StatusType statusType, String entity) {
+        super(status + ":" + entity);
+        this.status = status;
+        this.statusInfo = statusType;
+        this.entity = entity;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public Response.StatusType getStatusInfo() {
+        return statusInfo;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+
+}
