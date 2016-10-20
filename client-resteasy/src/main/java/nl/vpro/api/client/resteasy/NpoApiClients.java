43135ab4bffaf766fc33f577c58d859d3321f7e8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-20 15:20:28 +0000

    updated shared.

--- client-resteasy/src/main/java/nl/vpro/api/client/resteasy/NpoApiClients.java
+++ client-resteasy/src/main/java/nl/vpro/api/client/resteasy/NpoApiClients.java
@@ -2,11 +2,9 @@ package nl.vpro.api.client.resteasy;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Proxy;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.client.jaxrs.ClientHttpEngine;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
@@ -21,7 +19,6 @@ import nl.vpro.api.rs.v3.profile.ProfileRestService;
 import nl.vpro.api.rs.v3.schedule.ScheduleRestService;
 import nl.vpro.api.rs.v3.schedule.ScheduleRestServiceWithDefaults;
 import nl.vpro.resteasy.JacksonContextResolver;
-import nl.vpro.util.LeaveDefaultsProxyHandler;
 import nl.vpro.util.ReflectionUtils;
 
 @Named
@@ -37,8 +34,6 @@ public class NpoApiClients extends AbstractApiClient implements  NpoApiClientsMB
     private ScheduleRestServiceWithDefaults scheduleRestServiceProxy;
     private ProfileRestService profileRestServiceProxy;
 
-	private final String baseUrl;
-
     @Inject
     public NpoApiClients(
         @Named("npo-api.baseUrl")
@@ -52,9 +47,8 @@ public class NpoApiClients extends AbstractApiClient implements  NpoApiClientsMB
         @Named("npo-api.connectionTimeout")
         Integer connectionTimeout
     ) {
-		super(connectionTimeout, 16, 3);
+		super(apiBaseUrl + "api", connectionTimeout, 16, 3);
         this.authentication = new ApiAuthenticationRequestFilter(apiKey, secret, origin);
-        baseUrl = apiBaseUrl + "api";
 
 
 
@@ -140,50 +134,14 @@ public class NpoApiClients extends AbstractApiClient implements  NpoApiClientsMB
         return authentication;
     }
 
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    protected String getInfo() {
-        return getBaseUrl() + "/";
-    }
-
 	@Override
 	public String toString() {
 		return super.toString() + " " + baseUrl;
 	}
 
-    private <T, S> T build(ClientHttpEngine engine, Class<T> service, Class<S> restEasyService) {
-        T proxy;
-        if (restEasyService == null) {
-            proxy = builderResteasy(engine, service);
-        } else {
-            S resteasy = builderResteasy(engine, restEasyService);
-            proxy = (T) Proxy.newProxyInstance(NpoApiClients.class.getClassLoader(),
-                new Class[]{restEasyService, service}, new LeaveDefaultsProxyHandler(resteasy));
-        }
 
-        return
-            ErrorAspect.proxyErrors(
-                LOG,
-                NpoApiClients.this::getInfo,
-                service,
-                proxy);
-    }
-
-    private <T> T build(ClientHttpEngine engine, Class<T> service) {
-        return build(engine, service, null);
-    }
-
-    private <T> T builderResteasy(ClientHttpEngine engine, Class<T> service) {
-        return getTarget(engine)
-            .proxyBuilder(service)
-            .defaultConsumes(MediaType.APPLICATION_XML)
-            .defaultProduces(MediaType.APPLICATION_XML)
-            .build();
-    }
-
-    private ResteasyWebTarget getTarget(ClientHttpEngine engine) {
+    @Override
+    protected ResteasyWebTarget getTarget(ClientHttpEngine engine) {
         ResteasyClient client =
             new ResteasyClientBuilder()
                 .httpEngine(engine)
