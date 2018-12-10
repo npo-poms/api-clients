e2bd8839e7d8c60847bc9cca9a923368a6d32251
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-12-10 11:26:15 +0000

    Test case failures.

--- client-resteasy/src/test/java/nl/vpro/api/client/utils/SwaggerTest.java
+++ client-resteasy/src/test/java/nl/vpro/api/client/utils/SwaggerTest.java
@@ -2,6 +2,8 @@ package nl.vpro.api.client.utils;
 
 import org.junit.Test;
 
+import nl.vpro.util.Version;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -17,9 +19,9 @@ public class SwaggerTest {
 
         assertThat(Swagger.getVersion("5.6.2.${builder.vcs.number}", null)).isEqualTo("5.6.2");
 
-        assertThat(Swagger.getVersionNumber("5.6-SNAPSHOT.${builder.vcs.number}", null)).isEqualTo(5.6f);
+        assertThat(Swagger.getVersionNumber("5.6-SNAPSHOT.${builder.vcs.number}", null)).isEqualTo(Version.of(5, 6));
 
-        assertThat(Swagger.getVersionNumber("5.6.2.${builder.vcs.number}", null)).isEqualTo(5.602f);
+        assertThat(Swagger.getVersionNumber("5.6.2.${builder.vcs.number}", null)).isEqualTo(Version.of(5, 6, 2));
 
 
 
