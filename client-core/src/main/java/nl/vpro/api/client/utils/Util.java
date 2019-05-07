e698e31ed2a9f14885d6981d04fa8ef824b6dd44
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-07 13:32:24 +0200

    Removed unused methods

--- client-core/src/main/java/nl/vpro/api/client/utils/Util.java
+++ client-core/src/main/java/nl/vpro/api/client/utils/Util.java
@@ -5,15 +5,14 @@
 package nl.vpro.api.client.utils;
 
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Roelof Jan Koekoek
@@ -23,15 +22,7 @@ class Util {
 
     private static final String RFC822 = "EEE, dd MMM yyyy HH:mm:ss z";
 
-    public static String sha256(String input) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-            return toString(hash);
-        } catch(Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+    private static final TimeZone GMT = TimeZone.getTimeZone("GMT");
 
     public static String hmacSHA256(String privateKey, String data) {
         try {
@@ -46,31 +37,8 @@ class Util {
 
     public static String rfc822(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat(RFC822, Locale.US);
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        sdf.setTimeZone(GMT);
         return sdf.format(date);
     }
 
-    public static Date rfc822(String date) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat(RFC822, Locale.US);
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-        return sdf.parse(date);
-    }
-
-    private static String toString(byte[] hash) {
-        StringBuilder hexString = new StringBuilder();
-
-        for(int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if(hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-
-        return hexString.toString();
-    }
-
-    private static String trimAndReplaceNull(String optional) {
-        return (StringUtils.isNotBlank(optional) ? optional.trim() : UUID.randomUUID().toString());
-    }
 }
