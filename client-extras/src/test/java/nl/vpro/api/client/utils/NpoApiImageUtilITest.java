4fabe7963df6ef1d387190b17bc2f3de2559f871
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-24 10:55:19 +0200

    Fixed test case.

--- client-extras/src/test/java/nl/vpro/api/client/utils/NpoApiImageUtilITest.java
+++ client-extras/src/test/java/nl/vpro/api/client/utils/NpoApiImageUtilITest.java
@@ -1,9 +1,10 @@
 package nl.vpro.api.client.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import nl.vpro.util.ConfigUtils;
-import org.junit.Ignore;
+
 import org.junit.Test;
+
+import nl.vpro.util.ConfigUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,14 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NpoApiImageUtilITest {
 
     @Test
-    @Ignore("Does not yet reliably work on production")
     public void getGetSize() {
 
         NpoApiImageUtil util = new NpoApiImageUtil("https://images-dev.poms.omroep.nl/");
-
         ConfigUtils.configuredInHome(util, "apiclient.properties");
-
         log.info("Testing for {}", util.getBaseUrl());
-        assertThat(util.getSize("urn:image:706133").get()).isEqualTo(213356L);
+        assertThat(util.getSize("urn:vpro:image:706133").get()).isEqualTo(213356L);
     }
 }
