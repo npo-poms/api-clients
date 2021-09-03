5d524d625da71f80dcad44472fb218d0bcb5c8ca
Michiel Meeuwissen <git@meeuw.org>
2021-09-03 14:36:02 +0200

    comments.

--- client-core/src/main/java/nl/vpro/api/client/utils/Swagger.java
+++ client-core/src/main/java/nl/vpro/api/client/utils/Swagger.java
@@ -10,9 +10,8 @@ import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import com.fasterxml.jackson.core.*;
@@ -38,16 +37,18 @@ public class Swagger {
         try {
             ObjectMapper mapper = new ObjectMapper();
             JsonFactory factory = new JsonFactory();
-            HttpClient client = HttpClientBuilder.create().build();
-
-            URI url = URI.create(baseUrl + "/swagger.json");
-            HttpUriRequest request = new HttpGet(url);
-            request.addHeader(Headers.NPO_DATE, "CacheBust-" + UUID.randomUUID());
-            try (InputStream stream = client.execute(request).getEntity().getContent()) {
-                JsonParser jp = factory.createParser(stream);
-                JsonNode swagger = mapper.readTree(jp);
-                String versionString = swagger.get("info").get("version").asText();
-                return VersionResult.builder().version(getVersion(versionString, defaultVersion)).available(true).build();
+            try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+                URI url = URI.create(baseUrl + "/swagger.json");
+                HttpUriRequest request = new HttpGet(url);
+                request.addHeader(Headers.NPO_DATE, "CacheBust-" + UUID.randomUUID()); // Cloudfront includes npo date as a cache key header.
+                try (CloseableHttpResponse response = client.execute(request)) {
+                    try (InputStream stream = response.getEntity().getContent()) {
+                        JsonParser jp = factory.createParser(stream);
+                        JsonNode swagger = mapper.readTree(jp);
+                        String versionString = swagger.get("info").get("version").asText();
+                        return VersionResult.builder().version(getVersion(versionString, defaultVersion)).available(true).build();
+                    }
+                }
             }
         } catch (JsonParseException jpe) {
             log.warn(jpe.getMessage());
