dd6a367d8a7c5397e8e9899d76c37c977835a115
Michiel Meeuwissen <git@meeuw.org>
2022-10-28 10:50:14 +0200

    API-560 test case.

---
+++ frontend-api-client/src/test/java/nl/vpro/api/client/frontend/NpoApiClientsFilterTest.java
@@ -0,0 +1,14 @@
+package nl.vpro.api.client.frontend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NpoApiClientsFilterTest {
+
+    @Test
+    public void instantiate() {
+        new NpoApiClientsFilter();
+    }
+
+}
